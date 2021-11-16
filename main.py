9e1dd968b771b49295371cf19414a7a34843bd97
javierpb275 <javierpb275@gmail.com>
2021-11-16 19:32:53 +0100

    Working With Files In Python

---
+++ main.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    test_file = open('test.txt')
+    '''
+    print(test_file)#<_io.TextIOWrapper name='test.txt' mode='r' encoding='cp1252'>
+    print(test_file.read())#Hello I am the 'test' txt.              4: How are you doing?
+    test_file.seek(0)
+    print(test_file.read())#Hello I am the 'test' txt.              4: How are you doing?
+
+    print(test_file.readline())#Hello I am the 'test' txt.
+    print(test_file.readline())
+    print(test_file.readline())# :)
+'''
+    print(test_file.readlines()) #["Hello I am the 'test' txt.\n", '\n', ':)\n', 'how are you doing?']
+
+    test_file.close()
