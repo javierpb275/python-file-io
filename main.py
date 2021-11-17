787dd630d3d9e256d4cc4fdc15c9acfc96dbec43
javierpb275 <javierpb275@gmail.com>
2021-11-17 20:48:52 +0100

    read, write, append

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 if __name__ == '__main__':
-    test_file = open('test.txt')
+    with open('happy.txt', mode='a') as my_file:
+        text = my_file.write(':)')
+        print(text)
+
+
     '''
+    test_file = open('test.txt')
+
     print(test_file)#<_io.TextIOWrapper name='test.txt' mode='r' encoding='cp1252'>
     print(test_file.read())#Hello I am the 'test' txt.              4: How are you doing?
     test_file.seek(0)
@@ -9,7 +15,8 @@ if __name__ == '__main__':
     print(test_file.readline())#Hello I am the 'test' txt.
     print(test_file.readline())
     print(test_file.readline())# :)
-'''
+
     print(test_file.readlines()) #["Hello I am the 'test' txt.\n", '\n', ':)\n', 'how are you doing?']
 
     test_file.close()
+    '''
